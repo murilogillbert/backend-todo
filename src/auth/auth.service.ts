4a5b99effd43d12edfee5fb551cafc365b2ce22a
murilogillbert <murilo.gillbert@outlook.com>
2025-01-28 20:58:29 -0400

    Inslatando brcriptjs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
