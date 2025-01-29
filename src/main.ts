8404eb8349ae5ca490834a3e4f1dc87aee96f6d9
murilogillbert <murilo.gillbert@outlook.com>
2025-01-28 22:55:34 -0400

    Adição de mais fontes

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,23 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configurar CORS para aceitar requisições do frontend
+  // Lista de URLs permitidas
+  const allowedOrigins = [
+    'https://frontend-wheat-phi-56.vercel.app',
+    'https://frontend-git-main-murilogillberts-projects.vercel.app',
+    'https://frontend-ejrfsp5lj-murilogillberts-projects.vercel.app',
+  ];
+
+  // Configuração do CORS
   app.enableCors({
-    origin: 'https://frontend-wheat-phi-56.vercel.app', // URL do frontend hospedado no Vercel
+    origin: (origin, callback) => {
+      // Permitir origens específicas ou nenhuma origem (Postman, por exemplo)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Permite cookies, se necessário
   });
