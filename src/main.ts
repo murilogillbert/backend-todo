41e7c315761ddb0dc574bd7f8803ab9486462b90
murilogillbert <murilo.gillbert@outlook.com>
2025-01-28 21:25:23 -0400

    cors especifico

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Corrigindo CORS para permitir todas as origens ou apenas o frontend no Vercel
+  // Configurar CORS para permitir apenas o frontend hospedado no Vercel
   app.enableCors({
-    origin: ['*'
+    origin: [
+      'https://frontend-wheat-phi-56.vercel.app', // URL do frontend
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true, // Se estiver usando cookies/autenticação JWT
+    credentials: true, // Permite cookies e autenticação via JWT
   });
 
   await app.listen(process.env.PORT || 8080);
